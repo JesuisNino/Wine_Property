b313bd08eecbc693b228af63fd5123417a23861b
JesuisNino <74672645+JesuisNino@users.noreply.github.com>
2021-04-26 21:06:15 +0100

    4.26 night

--- src/uk/ac/sheffield/assignment2021/gui/WineSampleBrowserPanel.java
+++ src/uk/ac/sheffield/assignment2021/gui/WineSampleBrowserPanel.java
@@ -1,8 +1,13 @@
 package uk.ac.sheffield.assignment2021.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.ac.sheffield.assignment2021.codeprovided.AbstractWineSampleCellar;
 import uk.ac.sheffield.assignment2021.codeprovided.SubQuery;
 import uk.ac.sheffield.assignment2021.codeprovided.WineProperty;
+import uk.ac.sheffield.assignment2021.codeprovided.WineSample;
+import uk.ac.sheffield.assignment2021.codeprovided.WineType;
 import uk.ac.sheffield.assignment2021.codeprovided.gui.AbstractWineSampleBrowserPanel;
 
 public class WineSampleBrowserPanel extends AbstractWineSampleBrowserPanel {
@@ -19,21 +24,30 @@ public class WineSampleBrowserPanel extends AbstractWineSampleBrowserPanel {
     	buttonClearFilters.addActionListener(e -> {
     		clearFilters();
     	});
-    	comboWineTypes.addActionListener(comboWineTypes);
     }
 
     @Override
     public void addFilter() {
         // TODO implement
-    	WineProperty wineProperty = WineProperty.fromFileIdentifier((String)comboQueryProperties.getSelectedItem());
+    	WineProperty wineProperty = WineProperty.fromName((String)comboQueryProperties.getSelectedItem());
     	String operator = (String)comboOperators.getSelectedItem();
     	Double values = Double.valueOf(value.getText());
+    	
     	subQueryList.add(new SubQuery(wineProperty,operator,values));
+    	String subQueriesText = subQueryList.toString().replace("[","").replace("]","").replace(",", ";");
+    	
+    	subQueriesTextArea.setText(subQueriesText);
+    	statisticsTextArea.setText(operator);
+    	filteredWineSamplesTextArea.setText(filteredWineSampleList.toString());
     }
 
     @Override
     public void clearFilters() {
         // TODO implement
+    	subQueryList.clear();
+    	subQueriesTextArea.setText(null);
+    	statisticsTextArea.setText(null);
+    	filteredWineSamplesTextArea.setText(null);
     }
 
     @Override
@@ -49,5 +63,15 @@ public class WineSampleBrowserPanel extends AbstractWineSampleBrowserPanel {
     @Override
     public void executeQuery() {
         // TODO implement
+//    	 for (SubQuery subQuery : subQueryList)
+//         {
+//    		 for (int i=0;i<cellar.getWineSampleCount(WineType.ALL);i++) {
+//    	            if(subQuery.wineMatchesSubQuery(filteredWineSampleList.get(i)))
+//    	            	filteredWineSampleList.remove(i);
+//    	            }
+//
+//                
+//    	}
+       
     }
 }
