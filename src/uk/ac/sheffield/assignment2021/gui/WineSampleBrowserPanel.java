df9e818cb61607e705da8efbc526ad6d1dbb8625
JesuisNino <74672645+JesuisNino@users.noreply.github.com>
2021-04-27 23:11:10 +0100

    4.27 night

--- src/uk/ac/sheffield/assignment2021/gui/WineSampleBrowserPanel.java
+++ src/uk/ac/sheffield/assignment2021/gui/WineSampleBrowserPanel.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import uk.ac.sheffield.assignment2021.codeprovided.AbstractWineSampleCellar;
+import uk.ac.sheffield.assignment2021.codeprovided.Query;
 import uk.ac.sheffield.assignment2021.codeprovided.SubQuery;
 import uk.ac.sheffield.assignment2021.codeprovided.WineProperty;
 import uk.ac.sheffield.assignment2021.codeprovided.WineSample;
@@ -11,67 +12,105 @@ import uk.ac.sheffield.assignment2021.codeprovided.WineType;
 import uk.ac.sheffield.assignment2021.codeprovided.gui.AbstractWineSampleBrowserPanel;
 
 public class WineSampleBrowserPanel extends AbstractWineSampleBrowserPanel {
-    public WineSampleBrowserPanel(AbstractWineSampleCellar cellar) {
-        super(cellar);
-    }
+	public WineSampleBrowserPanel(AbstractWineSampleCellar cellar) {
+		super(cellar);
+	}
 
-    @Override
-    public void addListeners() {
-        // TODO implement
-    	buttonAddFilter.addActionListener(e -> {
-    		addFilter();
-    	});
-    	buttonClearFilters.addActionListener(e -> {
-    		clearFilters();
-    	});
-    }
+	@Override
+	public void addListeners() {
+		// TODO implement
+		buttonAddFilter.addActionListener(e -> {
+			addFilter();
+			executeQuery();
+		});
+		buttonClearFilters.addActionListener(e -> {
+			clearFilters();
+		});
+		comboWineTypes.addActionListener(e -> {
+			executeQuery();
+		});
+	}
 
-    @Override
-    public void addFilter() {
-        // TODO implement
-    	WineProperty wineProperty = WineProperty.fromName((String)comboQueryProperties.getSelectedItem());
-    	String operator = (String)comboOperators.getSelectedItem();
-    	Double values = Double.valueOf(value.getText());
-    	
-    	subQueryList.add(new SubQuery(wineProperty,operator,values));
-    	String subQueriesText = subQueryList.toString().replace("[","").replace("]","").replace(",", ";");
-    	
-    	subQueriesTextArea.setText(subQueriesText);
-    	statisticsTextArea.setText(operator);
-    	filteredWineSamplesTextArea.setText(filteredWineSampleList.toString());
-    }
+	@Override
+	public void addFilter() {
+		// TODO implement
+		WineProperty wineProperty = WineProperty.fromName((String) comboQueryProperties.getSelectedItem());
+		String operator = (String) comboOperators.getSelectedItem();
+		Double values = Double.valueOf(value.getText());
 
-    @Override
-    public void clearFilters() {
-        // TODO implement
-    	subQueryList.clear();
-    	subQueriesTextArea.setText(null);
-    	statisticsTextArea.setText(null);
-    	filteredWineSamplesTextArea.setText(null);
-    }
+		subQueryList.add(new SubQuery(wineProperty, operator, values));
+		String subQueriesText = subQueryList.toString().replace("[", "").replace("]", "").replace(",", ";");
 
-    @Override
-    public void updateStatistics() {
-        // TODO implement
-    }
+		subQueriesTextArea.setText(subQueriesText);
+		statisticsTextArea.setText(operator);
+	}
 
-    @Override
-    public void updateWineDetailsBox() {
-        // TODO implement
-    }
+	@Override
+	public void clearFilters() {
+		// TODO implement
+		subQueryList.clear();
+		subQueriesTextArea.setText(null);
+		statisticsTextArea.setText(null);
+		filteredWineSamplesTextArea.setText(null);
+	}
 
-    @Override
-    public void executeQuery() {
-        // TODO implement
-//    	 for (SubQuery subQuery : subQueryList)
-//         {
-//    		 for (int i=0;i<cellar.getWineSampleCount(WineType.ALL);i++) {
-//    	            if(subQuery.wineMatchesSubQuery(filteredWineSampleList.get(i)))
-//    	            	filteredWineSampleList.remove(i);
-//    	            }
-//
-//                
-//    	}
-       
-    }
+	@Override
+	public void updateStatistics() {
+		// TODO implement
+	}
+
+	@Override
+	public void updateWineDetailsBox() {
+		// TODO implement
+	}
+
+	@Override
+	public void executeQuery() {
+		// TODO implement
+		filteredWineSamplesTextArea.setText(null);
+		List<Query> queries = new ArrayList<>();
+		switch ((String) comboWineTypes.getSelectedItem()) {
+		case "RED":
+			queries.add(new Query(subQueryList, WineType.RED));
+			break;
+
+		case "WHITE":
+			queries.add(new Query(subQueryList, WineType.WHITE));
+			break;
+
+		case "ALL":
+			queries.add(new Query(subQueryList, WineType.ALL));
+			break;
+		}
+
+		for (Query query : queries) {
+			filteredWineSampleList = query.executeQuery(cellar);
+		}
+
+		filteredWineSamplesTextArea.append("WineType");
+		filteredWineSamplesTextArea.append("        ");
+		filteredWineSamplesTextArea.append("ID");
+		filteredWineSamplesTextArea.append("        ");
+
+		for (int i = 0; i < WineProperty.values().length; i++) {
+			filteredWineSamplesTextArea.append(WineProperty.values()[i].toString());
+			filteredWineSamplesTextArea.append("        ");
+		}
+
+		int line = 0;
+
+		while (line < filteredWineSampleList.size()) {
+			filteredWineSamplesTextArea.append("\n");
+			filteredWineSamplesTextArea.append(filteredWineSampleList.get(line).getWineType().toString());
+			filteredWineSamplesTextArea.append("        ");
+			filteredWineSamplesTextArea.append(String.valueOf(filteredWineSampleList.get(line).getId()));
+			filteredWineSamplesTextArea.append("        ");
+			for (int i = 0; i < WineProperty.values().length; i++) {
+				filteredWineSamplesTextArea.append((String.valueOf(filteredWineSampleList.get(line).getProperty(WineProperty.values()[i]))));
+				filteredWineSamplesTextArea.append("        ");
+			}
+			line++;
+		}
+
+	}
 }
