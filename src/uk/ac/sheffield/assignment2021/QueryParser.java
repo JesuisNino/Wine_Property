9d3339bec01262fcb4b506ab3f668006651a2237
JesuisNino <74672645+JesuisNino@users.noreply.github.com>
2021-04-27 11:49:10 +0100

    fix queryParser 4.27 morning

--- src/uk/ac/sheffield/assignment2021/QueryParser.java
+++ src/uk/ac/sheffield/assignment2021/QueryParser.java
@@ -13,37 +13,59 @@ public class QueryParser extends AbstractQueryParser {
     @Override
     public List<Query> readQueries(List<String> queryTokens) throws IllegalArgumentException {
         // TODO implement
-    	List<Query> query = new ArrayList<>();
+    	List<Query> queries = new ArrayList<>();
     	List<SubQuery> subQueryList = new ArrayList<>();
-       
+    	List<Integer> countSelect = new ArrayList<>();
+    	List<Integer> countWhere = new ArrayList<>();
+    	
     	WineProperty wineProperty = null;
         String operator = null;
         Double value = null;
-    	
-        for(int i=queryTokens.indexOf("where")+1;i<queryTokens.size();i++) {
-    		if(SubQuery.isValidOperator(queryTokens.get(i))) {
-    			wineProperty = WineProperty.fromFileIdentifier(queryTokens.get(i-1));
-    			operator = queryTokens.get(i);
-    			value = Double.valueOf(queryTokens.get(i+1));
-    			subQueryList.add(new SubQuery(wineProperty,operator,value));
-    		}	
-    	}
-    	
-        WineType wineType = null;
-    	if(queryTokens.contains("red") && queryTokens.contains("white")) {
-    		wineType = WineType.ALL;
-    		query.add(new Query(subQueryList,wineType));
-    	}else if(queryTokens.contains("red") && !queryTokens.contains("white")) {
-    		wineType = WineType.RED;
-    		query.add(new Query(subQueryList,wineType));
-    	}else if(queryTokens.contains("white") && !queryTokens.contains("red")){
-    		wineType = WineType.WHITE;
-    		query.add(new Query(subQueryList,wineType));
-    	}
-    	
-    	if(wineProperty == null||operator == null||value == null||wineType == null) {
-    		throw new IllegalArgumentException("Entered the wrong sentence!");
-    	}
-    	return query;
+        WineType type = null;
+        
+        for(int i=0;i<queryTokens.size();i++) {
+        	if(queryTokens.get(i).equals("select"))countSelect.add(i);
+        	if(queryTokens.get(i).equals("where"))countWhere.add(i);
+        }
+        	        
+        int index = 0;
+       
+        for(int i=0;i<queryTokens.size();i++) {           
+              	        	
+        	if(queryTokens.get(i).equalsIgnoreCase("red") && type != WineType.WHITE) {
+        		type = WineType.RED;       		 	
+        	}else if(queryTokens.get(i).equalsIgnoreCase("white") && type != WineType.RED){
+        		type = WineType.WHITE;
+        	}else if(queryTokens.get(i).equalsIgnoreCase("red") && type == WineType.WHITE) {
+        		type = WineType.ALL;
+        	}else if(queryTokens.get(i).equalsIgnoreCase("white") && type == WineType.RED) {
+        		type = WineType.ALL;
+        	}
+        	
+            		
+        	if(SubQuery.isValidOperator(queryTokens.get(i))) {            			
+        		wineProperty = WineProperty.fromFileIdentifier(queryTokens.get(i-1));   			
+        		operator = queryTokens.get(i);          			
+        		value = Double.valueOf(queryTokens.get(i+1));		
+        		subQueryList.add(new SubQuery(wineProperty,operator,value));            	
+        	}
+        	             	
+        	
+        	
+        	if(i==countSelect.get(index+1) || i==queryTokens.size()-1) {
+        		if(wineProperty == null||operator == null||value == null||type == null) {
+            		throw new IllegalArgumentException("Entered the wrong sentence!");
+            	}
+        		System.out.println(subQueryList);
+        		System.out.println(type);
+        		queries.add(new Query(subQueryList,type));	
+        		subQueryList = new ArrayList<>();
+        		type = null;
+        		while(index < countSelect.size()-2)index++;
+        	}
+        	
+        }
+      	
+    	return queries;
     }
 }
